64406cc34b22c96b9b559f26046258cd966747d8
Taticalzero <52974709+Taticalzero@users.noreply.github.com>
2020-03-09 09:02:43 -0300

    Update java.app
    Foi acrescentado um laço de repetição antes do switch case !

--- trab03/trab03/src/app/App.java
+++ trab03/trab03/src/app/App.java
@@ -6,60 +6,65 @@ public class App {
     public static void main(String[] args) throws Exception {
         Poupancaconta conta = new Poupancaconta();
         Scanner sc = new Scanner(System.in);
+        
+        int num = 1;
 
         double dinheiro = 0.0;
 
         System.out.println("\t\t[]======================[]");
         System.out.println("\t\t[] Número da conta   []");
         System.out.print("\t\t[]======================[]");
+        
         int numero = sc.nextInt();
         conta.setnumconta(numero);
 
+        while (num != 0) { 
 
-        
-        System.out.println("\t\t[] Informe a operação []");
-        System.out.print("\t\t[]======================[]");
-        System.out.println("\t\t[]    0: Sair         [] ");
-        System.out.print("\t\t[]======================[]");
-        System.out.println("\t\t[]    1: Sacar        []");
-        System.out.print("\t\t[]======================[]");
-        System.out.println("\t\t[]   2: Deposito      []");
-        System.out.print("\t\t[]======================[]");
-        int num = sc.nextInt();
+            System.out.println("\t\t[] Informe a operação []");
+            System.out.print("\t\t[]======================[]");
+            System.out.println("\t\t[]    0: Sair         [] ");
+            System.out.print("\t\t[]======================[]");
+            System.out.println("\t\t[]    1: Sacar        []");
+            System.out.print("\t\t[]======================[]");
+            System.out.println("\t\t[]   2: Deposito      []");
+            System.out.print("\t\t[]======================[]");
+            
+            num = sc.nextInt();
 
-        switch (num) {
-            case 1:
-                System.out.println("\t\t---------------------------------------");
-                System.out.println("\t\t---------- 1 = SACAR ------------------");
-                System.out.println("\t\t---------------------------------------");
+            switch (num) {
+                case 1:
+                    System.out.println("\t\t---------------------------------------");
+                    System.out.println("\t\t---------- 1 = SACAR ------------------");
+                    System.out.println("\t\t---------------------------------------");
 
-                dinheiro = sc.nextDouble();
-                conta.sacar(dinheiro);
+                    dinheiro = sc.nextDouble();
+                    conta.sacar(dinheiro);
 
-                break;
+                    break;
 
-            case 2:
-                System.out.println("\t\t---------------------------------------");
-                System.out.println("\t\t---------- 2 = DEPOSITAR ------------------");
-                System.out.println("\t\t---------------------------------------");
-                
-                dinheiro = sc.nextDouble();
-                conta.depositar(dinheiro);
+                case 2:
+                    System.out.println("\t\t---------------------------------------");
+                    System.out.println("\t\t---------- 2 = DEPOSITAR ------------------");
+                    System.out.println("\t\t---------------------------------------");
+                    
+                    dinheiro = sc.nextDouble();
+                    conta.depositar(dinheiro);
 
-                break;
-        
-            default:
-                System.out.println("\t\t---------------------------------------");
-                System.out.println("\t\t---------- 0 = SAIR ------------------");
-                System.out.println("\t\t---------------------------------------");
+                    break;
+            
+                case 0:
+                    System.out.println("\t\t---------------------------------------");
+                    System.out.println("\t\t---------- 0 = SAIR ------------------");
+                    System.out.println("\t\t---------------------------------------");
 
-                break;
-        }
-        
-        System.out.println(conta);
-        System.out.println("\t\t---------------------------------------");
-        System.out.println("\t\t---------- VOCÊ SAIU DE SUA CONTA ------------------");
-        System.out.println("\t\t---------------------------------------");
+                    break;
+            }
+
+            System.out.println(conta);
+            System.out.println("\t\t---------------------------------------");
+            System.out.println("\t\t---------- VOCÊ SAIU DE SUA CONTA ------------------");
+            System.out.println("\t\t---------------------------------------");
+        }   
 
         sc.close();
     }
